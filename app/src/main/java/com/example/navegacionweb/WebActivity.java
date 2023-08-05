e01981098d38800ae1d81db5d689c563efbf269b
Luca <lucajeresanti@gmail.com>
2023-08-05 19:59:55 -0300

    Version final

--- app/src/main/java/com/example/navegacionweb/WebActivity.java
+++ app/src/main/java/com/example/navegacionweb/WebActivity.java
@@ -3,6 +3,8 @@ package com.example.navegacionweb;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
@@ -14,8 +16,11 @@ public class WebActivity extends AppCompatActivity {
         setContentView(R.layout.activity_web);
 
         WebView1 = (WebView)findViewById(R.id.WebView1);
-
         String URL = getIntent().getStringExtra("SitioWeb");
+
+        WebSettings webSettings = WebView1.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
         WebView1.setWebViewClient(new WebViewClient());
         WebView1.loadUrl(URL);
     }
